c79a4ba52053e72ce21477c93eefd756b4102bac
Jen Chow <jennego@gmail.com>
2019-08-24 21:59:41 -0700

    Redid background, fixed wave, laid out skills

--- src/components/plx.js
+++ src/components/plx.js
@@ -13,12 +13,14 @@ import stars from './stars.js'
 class Plx extends Component {
 
     render() {
+
+        
         
         return (
 
             <Parallax ref={ref => (this.parallax = ref)} pages={5} >
                 <ParallaxLayer offset={0} speed={0} style={{
-                    background:'linear-gradient(0deg, rgba(106,162,219,1) 0%,rgba(25,0,150,1) 55%)' }} />
+                    background:'linear-gradient(0deg, rgba(106,162,219,1) 0%, rgba(47,3,128,1) 55%)' }} />
                 <ParallaxLayer offset={0} speed={0}>
                     <svg id="svgCanvas" viewbox="0 0 2000 1000" preserveaspectratio="xMinYMin slice" width="100%" height="100%">
                         <g id="starGroup"></g>
@@ -27,8 +29,8 @@ class Plx extends Component {
             
                 <ParallaxLayer offset={1} speed={0} style={{background:'linear-gradient(0deg, rgba(192, 224, 247, 1) 0%,rgba(106, 162, 219, 1) 89%)'}} />
                 <ParallaxLayer offset={2} speed={0} style={{ background:'linear-gradient(180deg, rgba(192, 224, 247, 1) 52%, rgba(247, 227, 249, 1) 82%)' }} />
-                <ParallaxLayer offset={3} speed={0} style={{ background:'linear-gradient(180deg, rgba(247,227,249,1) 13%, rgba(11,112,199,1) 43%, rgba(19,92,198,1) 70%)' }} /> 
-                <ParallaxLayer offset={3} speed={0} >
+                <ParallaxLayer offset={3} speed={0} factor={1.2} style={{ background:'linear-gradient(180deg, rgba(1,88,113,1) 0%, rgba(8, 83, 193, 1) 63%)' }} /> 
+                <ParallaxLayer offset={2.1} speed={0} >
                     <div class="ocean">
                         <div class="wave"></div>
                         <div class="wave"></div>
@@ -36,7 +38,7 @@ class Plx extends Component {
                 </ParallaxLayer>
 
 
-                <ParallaxLayer offset={4} speed={0} style={{ background:'linear-gradient(180deg, rgba(19, 92, 198, 1) 0%, rgba(25, 0, 150, 1) 65%)' }} />
+                <ParallaxLayer offset={4} speed={0} style={{ background:'linear-gradient(180deg, rgba(8,83,193,1) 0%, rgba(47,3,128,1) 92%)' }} />
 
                 <ParallaxLayer offset={0.3} speed={0.2}> 
                     <div style={{ textAlign: 'center', color: '#fff' }}>
@@ -50,14 +52,27 @@ class Plx extends Component {
                     <img src={`${nep}`} style={{ display: 'block', width: '20%', marginLeft: '10%', marginTop: '8%' }} />
                 </ParallaxLayer>
 
+                <ParallaxLayer offset={1} speed={1} style={{ opacity: 0.7 }}>
+                    <img src={`${HTML5}`} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+                    <img src={`${CSS3}`} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+                    <img src={`${js}`} style={{ display: 'block', width: '10%', marginLeft: '40%' }} />
+                </ParallaxLayer>
+                
+
                 <ParallaxLayer offset={1} speed={0.2}>
                     <div style={{ textAlign: 'center' }}>
                         <h1 className="title"> Skills </h1>
-                        <p className="statement"> I have skillz   </p>
-                        <ul>
-                        <li> Software: Atom, Visual Studio Code, Microsoft Office, Dreamweaver, Photoshop, After Effects, Premiere Pro </li>
-                        <li> Languages and Frameworks: HTML, CSS, JavaScript,  Ruby, Ruby on Rails, Node.js/Express.js, Bootstrap </li>
-                        <li> Other Technical: Git, Github, Bash CLI, PostgresSQL, Heroku, WordPress, MacOSX/Windows </li>
+    
+                        
+                        <ul class="list-group col-3">
+                            <li class="list-group-item"> <h4> Languages / Frameworks </h4> </li>
+                            <li class="list-group-item">HTML/CSS</li>
+                            <li class="list-group-item">JavaScript</li>
+                            <li class="list-group-item">Ruby</li>
+                            <li class="list-group-item">Ruby on Rails</li>
+                            <li class="list-group-item">React</li>
+                            <li class="list-group-item">Bootstrap and Other CSS UI packages</li>
+                            <li class="list-group-item">Node/Express </li>
                         </ul>
 
                     <p> This very page is made with Gatsby, React and React-Spring! <a href="https://github.com/jennego/digi-resume"> See code for this site on Github.</a></p>
@@ -65,22 +80,40 @@ class Plx extends Component {
 
                 </ParallaxLayer>
 
+
+                <ParallaxLayer offset={1} speed={0.3} style={{ opacity: 1.0 }}>
+
+                    <ul class="list-group col-3 offset-md-4" style={{marginTop: '6rem'}}>
+                        <li class="list-group-item"> <h4> Software </h4> </li>
+                        <li class="list-group-item">Atom, Visual Studio Code etc</li>
+                        <li class="list-group-item">Microsoft Office</li>
+                        <li class="list-group-item">Photoshop, After Effects, Premiere</li>
+                        <li class="list-group-item">Windows/MacOSX</li>
+                    </ul>
+                </ParallaxLayer> 
+
+                <ParallaxLayer offset={1} speed={0.4} style={{ opacity: 1.0 }}>
+
+                    <ul class="list-group col-3 offset-md-8" style={{ marginTop: '6rem' }}>
+                        <li class="list-group-item"> <h4>Other Technical </h4> </li>
+                        <li class="list-group-item">Git / Github</li>
+                        <li class="list-group-item">Bash CLI</li>
+                        <li class="list-group-item">Heroku</li>
+                        <li class="list-group-item">PostgreSQL</li>
+                        <li class="list-group-item">WordPress</li>
+                    </ul>
+                </ParallaxLayer> 
+
                 <ParallaxLayer offset={2} speed={0.8} style={{ opacity: 1.0 }}>
                     <img src={`${peggy}`} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
                 </ParallaxLayer> 
 
 
-                <ParallaxLayer offset={1} speed={1} style={{ opacity: 0.7 }}>
-                    <img src={`${HTML5}`} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-                    <img src={`${CSS3}`} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
-                    <img src={`${js}`} style={{ display: 'block', width: '10%', marginLeft: '40%' }} />
-                </ParallaxLayer>
 
 
                 <ParallaxLayer offset={2} speed={0.3}>
                     <div style={{ textAlign: 'center' }}>
                         <h1 className="title"> Education </h1>
-                        <p className="statement"> I have education!   </p>
                         <h3> CodeCore Bootcamp </h3>
                         <h3> Simon Fraser University </h3> 
                     </div>
