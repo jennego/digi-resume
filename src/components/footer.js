34b838cd9f251b916c03c3e9f6d21c9982cf7bc1
Jen Chow <jennego@gmail.com>
2019-08-26 12:52:15 -0700

    First deployment

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,35 +2,37 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div> 
-            <nav class="navbar navbar-expand-lg fixed-bottom navbar-dark">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav mx-auto   text-center">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#"> Resume <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://jenniferchow.ca">Portfolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://blog.jennego.com">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://github.com/jennego"> <i class="fab fa-github"></i> Github</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://linkedin.com/in/jennego"> <i class="fab fa-linkedin"></i> LinkedIn</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://instagram.com/thejennego"> <i class="fab fa-instagram"></i> Instagram</a>
-                        </li>
+        <div class="footer"> 
 
-                    </ul>
+
+            <nav class="navbar navbar-expand navbar-dark fixed-bottom ">
+                <div class="container justify-content-center">
+                    <div class="d-flex flex-column flex-wrap" id="navbarCollapse">
+                        <ul class="navbar-nav mx-auto text-center">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#"> Resume <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://jenniferchow.ca">Portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://blog.jennego.com">Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://github.com/jennego"> <i class="fab fa-github"></i> Github</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://linkedin.com/in/jennego"> <i class="fab fa-linkedin"></i> LinkedIn</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://instagram.com/thejennego"> <i class="fab fa-instagram"></i> Instagram</a>
+                            </li>
+                        </ul>
+                        <div class="navbar-text mx-auto"> <small> Copyright. 2019. Stars and planets from <a href="obsidiandawn.com"> Obsidian Dawn Photoshop Brushes</a>. Wave from <a href="https://codepen.io/tedmcdo/pen/PqxKXg">Ted McDonald on CodePen</a>.  </small> </div>
+                    </div>
                 </div>
             </nav>
+
         </div>
     )
 }
--- src/components/plx.js
+++ src/components/plx.js
@@ -22,7 +22,7 @@ class Plx extends Component {
         
         return (
 
-            <Parallax ref={ref => (this.parallax = ref)} pages={5} >
+            <Parallax ref={ref => (this.parallax = ref)} pages={5.2} >
                 <ParallaxLayer offset={0} speed={0} style={{
                     background:'linear-gradient(0deg, rgba(106,162,219,1) 0%, rgba(47,3,128,1) 55%)' }} />
                 <ParallaxLayer offset={0} speed={0}>
@@ -31,7 +31,7 @@ class Plx extends Component {
             
                 <ParallaxLayer offset={1} speed={0} style={{background:'linear-gradient(0deg, rgba(192, 224, 247, 1) 0%,rgba(106, 162, 219, 1) 89%)'}} />
                 <ParallaxLayer offset={2} speed={0} style={{ background:'linear-gradient(180deg, rgba(192, 224, 247, 1) 52%, rgba(247, 227, 249, 1) 82%)' }} />
-                <ParallaxLayer offset={3} speed={0} factor={1.2} style={{ background:'linear-gradient(180deg, rgba(1,88,113,1) 0%, rgba(8, 83, 193, 1) 63%)' }} /> 
+                <ParallaxLayer offset={3} speed={0} factor={1} style={{ background:'linear-gradient(180deg, rgba(1,88,113,1) 0%, rgba(8, 83, 193, 1) 63%)' }} /> 
                     <ParallaxLayer offset={2} speed={-0.10} style={{ opacity: 0.8 }}>
                     <img src={`${cloud1}`} style={{ display: 'block', width: '40%', marginLeft: '80%', marginTop: '1%' }} />
                     <img src={`${cloud1}`} style={{ display: 'block', width: '20%', marginLeft: '50%', marginBottom: '50%' }} />
@@ -59,10 +59,10 @@ class Plx extends Component {
                     <img src={`${nep}`} style={{ display: 'block', width: '20%', marginLeft: '10%', marginTop: '8%' }} />
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={1} speed={1} style={{ opacity: 0.4 }}>
-                    <img src={`${HTML5}`} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-                    <img src={`${CSS3}`} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
-                    <img src={`${js}`} style={{ display: 'block', width: '10%', marginLeft: '40%' }} />
+                <ParallaxLayer offset={1} speed={0.9} style={{ opacity: 0.4 }}>
+                    <img src={`${HTML5}`} style={{ display: 'block', width: '5%', marginLeft: '10%' }} />
+                    <img src={`${CSS3}`} style={{ display: 'block', width: '5%', marginLeft: '80%' }} />
+                    <img src={`${js}`} style={{ display: 'block', width: '5%', marginLeft: '40%' }} />
                 </ParallaxLayer>
 
                 <ParallaxLayer offset={1} speed={1.2} style={{ opacity: 0.4 }}>
@@ -75,6 +75,7 @@ class Plx extends Component {
                 <ParallaxLayer offset={1} speed={0.2}>
                     <div style={{ textAlign: 'center' }}>
                         <h1 className="title"> Skills </h1>
+                        
     
                         
                         <ul class="list-group col-3" style={{textAlign: 'left'}}>
@@ -87,9 +88,9 @@ class Plx extends Component {
                             <li class="list-group-item">Bootstrap and Other CSS UI packages</li>
                             <li class="list-group-item">Node/Express </li>
                         </ul>
-
-                    <p> This very page is made with Gatsby, React and React-Spring! <a href="https://github.com/jennego/digi-resume"> See code for this site on Github.</a></p>
                     </div>
+
+                
 
                 </ParallaxLayer>
 
@@ -116,8 +117,10 @@ class Plx extends Component {
                         <li class="list-group-item">WordPress</li>
                     </ul>
                 </ParallaxLayer> 
-
-                <ParallaxLayer offset={1} speed={2} style={{ opacity: 0.8 }}>
+                <ParallaxLayer offset={1.9} speed={0.2} style={{ opacity: 1.0 }}>
+                <p style={{marginTop:'20px', textAlign:'center'}}> This very page is made with Gatsby, React and React-Spring! <a href="https://github.com/jennego/digi-resume"> See code for this site on Github.</a></p>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0.9} speed={1} style={{ opacity: 0.5 }}>
                     <img src={`${cloud1}`} style={{ display: 'block', width: '20%', marginLeft: '50%', marginTop: '1%' }} />
                     <img src={`${cloud2}`} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
                     <img src={`${cloud1}`} style={{ display: 'block', width: '20%', marginLeft: '20%' }} />
@@ -136,15 +139,24 @@ class Plx extends Component {
                     <div style={{ textAlign: 'center' }}>
                         <h1 className="title"> Education </h1>
                         <h4> Full-Stack Web Development Certificate  </h4>
-                          <h4>  CodeCore Bootcamp | 2017 </h4>
+                        <h5>  CodeCore Bootcamp | 2017 </h5>
                         <p> 12 week, 60 hours/week, full-stack web development bootcamp. </p>
                         <h4> Bachelor of Arts </h4> 
-                        <h4> Simon Fraser University | 2011-2013 </h4>
-                        <p> >Major in English, Minor in History </p>
+                        <h5> Simon Fraser University | 2011-2013 </h5>
+                        <p> Major in English, Minor in History </p>
                     </div>
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={3} speed={0.3}>
+                <ParallaxLayer offset={3.0} speed={0.3}>
+                    <div style={{ textAlign: 'center', color: 'white' }}>
+                        <h1 className="title"> Portfolio</h1>
+                       
+
+                    </div>
+                </ParallaxLayer>
+    
+
+                <ParallaxLayer offset={3.6} speed={0.3}>
                     <div style={{ textAlign: 'center', color: 'white' }}>
                         <h1 className="title"> Hobbies </h1>
                         <p className="statement"> Away from the computer, I enjoy horseback riding, photography and being a weirdo.   </p>
@@ -153,33 +165,41 @@ class Plx extends Component {
                 </ParallaxLayer>
 
 
-                <ParallaxLayer offset={3.9} speed={0.3}>
+
+
+                <ParallaxLayer offset={4} speed={0.3}>
                     <div style={{ textAlign: 'center', color: 'white' }}>
                         <h1 className="title"> Contact </h1>
                         <form class="col-5 mx-auto" action="https://formspree.io/jennego@gmail.com" method="POST">
 
                         <div class="form-group">
                             <label for="email">Email address</label>
-                            <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="unicorn@exmaple.com" />
             
                         </div>
                         <div class="form-group">
                             <label for="name"> Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Name" />
+                            <input type="text" class="form-control" id="name" placeholder="Joe Smo" />
                         </div>
                         <div class="form-group">
                             <label class="form-check-label" for="text"> Your Message 
                             </label>
-                            <textarea class="form-control" rows="4" id="text" />
+                            <textarea class="form-control" rows="4" id="text" placeholder="I will buy you a pony and pay for all pony bills. Or a millon dollars." />
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
+                    <p class="container" style={{textAlign:'left'}}> I am located in Ladysmith on Vancouver Island, British Columbia, Canada but can work anywhere through the power of the internet! I am also close to Vancouver, BC, if necessary </p>
+                    
                     </div>
                 </ParallaxLayer>
 
+        
 
-                
         <Footer></Footer>
+     
+
+    
+                
         </Parallax> 
 
         )}
